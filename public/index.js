e278dd07742398657a03f916400671ea4d73b1b0
olvsivkov <1000listov@mail.ru>
2023-09-22 00:17:38 +0300

    I have written html markup and ts code that get the value from the html form

--- public/index.js
+++ public/index.js
@@ -1,2 +1,21 @@
 "use strict";
-console.log('hello world');
+const resultPerKilo = document.querySelector(".result-per-kilo");
+const BTNSubmit = document.querySelector(".BTNSubmit");
+const form = document.querySelector('form');
+function createElem(amount, weight) {
+    var _a;
+    const div = document.createElement('div');
+    div.innerHTML = `<p>цена ${amount} вес ${weight}</p><button>Close</button>`;
+    (_a = div.querySelector('button')) === null || _a === void 0 ? void 0 : _a.addEventListener('click', () => {
+        div.remove();
+    });
+    return div;
+}
+function submitForm() {
+    let numAmount = Number(form === null || form === void 0 ? void 0 : form.num_amount.value);
+    const numWeight = Number(form === null || form === void 0 ? void 0 : form.num_weight.value);
+    console.log(typeof numAmount);
+    resultPerKilo === null || resultPerKilo === void 0 ? void 0 : resultPerKilo.append(createElem(numAmount, numWeight));
+    //numAmount.value = null
+}
+BTNSubmit === null || BTNSubmit === void 0 ? void 0 : BTNSubmit.addEventListener('click', submitForm);
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,25 @@
-console.log('hello world')
+const resultPerKilo = document.querySelector(".result-per-kilo")
+const BTNSubmit = document.querySelector(".BTNSubmit")
+const form = document.querySelector('form');
+
+function createElem(amount:number, weight:number) {
+  const div: HTMLDivElement = document.createElement('div');
+  div.innerHTML = `<p>цена ${amount} вес ${weight}</p><button>Close</button>`;
+  div.querySelector('button')?.addEventListener('click', () => {
+    div.remove();
+  });
+  return div;
+}
+
+function submitForm() {
+  let numAmount: number = Number(form?.num_amount.value)
+  const numWeight: number = Number(form?.num_weight.value)
+  console.log(typeof numAmount)
+  resultPerKilo?.append(createElem(numAmount, numWeight))
+  //numAmount.value = null
+}
+
+BTNSubmit?.addEventListener('click', submitForm)
+
+
+
