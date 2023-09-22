df87f182088d418af64aa870e6ff018c3e6869f5
olvsivkov <1000listov@mail.ru>
2023-09-23 01:22:37 +0300

    added calculate functions to 100 gram and to 1000 gram

--- public/index.js
+++ public/index.js
@@ -1,21 +1,37 @@
 "use strict";
-const resultPerKilo = document.querySelector(".result-per-kilo");
-const BTNSubmit = document.querySelector(".BTNSubmit");
-const form = document.querySelector('form');
+var resultPerKilo = document.querySelector(".result-per-kilo");
+var BTNSubmit = document.querySelector(".BTNSubmit");
+var form = document.querySelector('form');
+// calculate functions
+var valueObject = {
+    numAmount: 0,
+    numWeight: 0
+};
+function getValueFromForm() {
+    valueObject.numAmount = form === null || form === void 0 ? void 0 : form.num_amount.value,
+        valueObject.numWeight = form === null || form === void 0 ? void 0 : form.num_weight.value;
+}
+function calcToOneHundredGrams() {
+    var valueToOneGram = valueObject.numAmount / valueObject.numWeight;
+    return +valueToOneGram.toFixed(2);
+}
+function calcToOneThousandGrams() {
+    var valueToThousandGram = (valueObject.numAmount / valueObject.numWeight) * 1000;
+    return +valueToThousandGram.toFixed(2);
+}
+// create info element after calculate values
 function createElem(amount, weight) {
     var _a;
-    const div = document.createElement('div');
-    div.innerHTML = `<p>цена ${amount} вес ${weight}</p><button>Close</button>`;
-    (_a = div.querySelector('button')) === null || _a === void 0 ? void 0 : _a.addEventListener('click', () => {
+    var div = document.createElement('div');
+    div.innerHTML = "<p>\u0446\u0435\u043D\u0430 \u0437\u0430 100 \u0433\u0440 ".concat(amount, " \u0440. \u0426\u0435\u043D\u0430 \u0437\u0430 1 \u043A\u0433 ").concat(weight, " \u0440.</p><button>Close</button>");
+    (_a = div.querySelector('button')) === null || _a === void 0 ? void 0 : _a.addEventListener('click', function () {
         div.remove();
     });
     return div;
 }
 function submitForm() {
-    let numAmount = Number(form === null || form === void 0 ? void 0 : form.num_amount.value);
-    const numWeight = Number(form === null || form === void 0 ? void 0 : form.num_weight.value);
-    console.log(typeof numAmount);
-    resultPerKilo === null || resultPerKilo === void 0 ? void 0 : resultPerKilo.append(createElem(numAmount, numWeight));
+    getValueFromForm();
+    resultPerKilo === null || resultPerKilo === void 0 ? void 0 : resultPerKilo.append(createElem(calcToOneHundredGrams(), calcToOneThousandGrams()));
     //numAmount.value = null
 }
 BTNSubmit === null || BTNSubmit === void 0 ? void 0 : BTNSubmit.addEventListener('click', submitForm);
--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,37 @@ const resultPerKilo = document.querySelector(".result-per-kilo")
 const BTNSubmit = document.querySelector(".BTNSubmit")
 const form = document.querySelector('form');
 
+interface IvalueFromForm {
+  numAmount: number,
+  numWeight: number
+}
+
+// calculate functions
+
+const valueObject: IvalueFromForm = {
+  numAmount: 0,
+  numWeight: 0
+}
+
+function getValueFromForm():void {
+    valueObject.numAmount = form?.num_amount.value,
+    valueObject.numWeight = form?.num_weight.value
+}
+function calcToOneHundredGrams(): number {
+  const valueToOneGram: number = valueObject.numAmount/valueObject.numWeight
+  return +valueToOneGram.toFixed(2) 
+}
+
+function calcToOneThousandGrams(): number  {
+  const valueToThousandGram: number = (valueObject.numAmount/valueObject.numWeight) * 1000
+  return +valueToThousandGram.toFixed(2) 
+}
+
+// create info element after calculate values
+
 function createElem(amount:number, weight:number) {
   const div: HTMLDivElement = document.createElement('div');
-  div.innerHTML = `<p>цена ${amount} вес ${weight}</p><button>Close</button>`;
+  div.innerHTML = `<p>цена за 100 гр ${amount} р. Цена за 1 кг ${weight} р.</p><button>Close</button>`;
   div.querySelector('button')?.addEventListener('click', () => {
     div.remove();
   });
@@ -12,10 +40,8 @@ function createElem(amount:number, weight:number) {
 }
 
 function submitForm() {
-  let numAmount: number = Number(form?.num_amount.value)
-  const numWeight: number = Number(form?.num_weight.value)
-  console.log(typeof numAmount)
-  resultPerKilo?.append(createElem(numAmount, numWeight))
+  getValueFromForm()
+  resultPerKilo?.append(createElem(calcToOneHundredGrams(),calcToOneThousandGrams()))
   //numAmount.value = null
 }
 
